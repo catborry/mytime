8e78e1845269e8b6d80f94d309edfb353dd46b63
catborry <2591981044@qq.com>
2019-04-01 09:43:07 +0800

    1

--- src/main/java/top/yyqdemao/mytime/utilMail/Mail.java
+++ src/main/java/top/yyqdemao/mytime/utilMail/Mail.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,29 +27,48 @@ public class Mail implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String sdate = "2018-02-10 07:00:00";
+        String sdate = "2019-04-02 07:00:00";
         SimpleDateFormat sf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
         Date date = sf.parse(sdate);
         Timer t=new Timer();
-        t.schedule(new TimerTask() {
+        t.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 try {
                     SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
                     //邮件发送人
                     simpleMailMessage.setFrom(MAIL_SENDER);
-                    //邮件接收人
-                    simpleMailMessage.setTo("554417780@qq.com");
+//                    //邮件接收人
+
+//                    simpleMailMessage.setTo("2591981044@qq.com");
+                    simpleMailMessage.setTo("2591981044@qq.com","2270989618@qq.com");
                     //邮件主题
                     simpleMailMessage.setSubject("起床啦!!!!");
+                    StringBuilder sb=new StringBuilder();
+                    LocalDateTime now = LocalDateTime.now();
+                    sb.append("现在时间是"+now.getYear()+"年"+now.getMonthValue()+"月"+now.getDayOfMonth()+"日  "+now.getHour()+"点"+now.getMinute()+"分"+now.getSecond()+"秒");
                     //邮件内容
-                    simpleMailMessage.setText("现在是"+LocalDateTime.now()+"还不起床!!!");
+                    simpleMailMessage.setText(sb+eat());
                     javaMailSender.send(simpleMailMessage);
+                    log.info("发送成功");
                 } catch (Exception e) {
                     log.error("邮件发送失败", e.getMessage());
                 }
+//                86400000
             }
         //86400000
         },date,86400000);
+    }
+
+    public String eat(){
+        String[] drink={"牛奶","酸奶","热水","豆浆","早餐奶"};
+        String[] eat={"饭","米粉","包子","油条","饼干"};
+        StringBuilder sb=new StringBuilder();
+        Random r=new Random();
+        sb.append("今天早上适合喝");
+        sb.append(drink[r.nextInt(drink.length)]);
+        sb.append(", 主食适合");
+        sb.append(eat[r.nextInt(eat.length)]);
+        return sb.toString();
     }
 }
